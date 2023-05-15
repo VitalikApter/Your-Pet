3448a3f5bc2b5d2c8492031cb728d9b13a3bd5c1
VitalikApter <aptvetal2014@gmail.com>
2023-05-15 23:08:55 +0300

    FavoriteNotices(Begin)

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 
+const {DB_HOST} = process.env;
 
 
 
-
-mongoose.connect("mongodb+srv://Vitalik:ANGELOCHEK2018@cluster0.82avjsn.mongodb.net/db-pets?retryWrites=true&w=majority")
+mongoose.connect(DB_HOST)
 .then(app.listen(3000, () => {
   console.log("Database connection successful");
 }))
